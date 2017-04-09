8f483d85fa8b55c44cb632fc00f596341ec8a864
Yuta Suzuki <bugs_bugs_bunny69@yahoo.co.jp>
2017-04-10 01:41:45 +0900

    update: create get users

--- controllers/user.js
+++ controllers/user.js
@@ -2,8 +2,13 @@ import express from 'express'
 import User from '../models/user'
 
 const router = express.Router()
-
 const UserModel = new User();
+
+router.get('/', function(req, res) {
+  UserModel.all(function (err, user) {
+    res.json({user})
+  })
+})
 
 router.get('/:id', function(req, res) {
   UserModel.get(req.params.id, function (err, user) {
--- models/user.js
+++ models/user.js
@@ -1,24 +1,3 @@
-// // Create new comment in your database and return its id
-// export const create = (user, text, cb) => {
-//   cb('12345')
-// }
-//
-// // Get a particular comment
-// export const get = (id, cb) => {
-//   cb(null, {id:id, text: 'Very nice example'})
-// }
-//
-// // Get all comments
-// export const all = (cb) => {
-//   cb(null, [])
-// }
-//
-// // Get all comments by a particular user
-// export const allByUser = (user, cb) => {
-//   cb(null, [])
-// }
-
-
 class User {
   create(user, text, cb) {
     cb('12345')
